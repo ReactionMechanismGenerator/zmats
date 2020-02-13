3701872c767b9c96bdc6fb2fcce740ff138b777a
Alon Grinberg Dana <alongd@mit.edu>
2020-02-13 15:42:01 -0500

    Added ZMatError to exceptions

---
+++ zmats/exceptions.py
@@ -0,0 +1,10 @@
+"""
+This module contains classes which extend Exception.
+"""
+
+
+class ZMatError(Exception):
+    """
+    An exception class for exceptional behavior that occurs while working with z matrices.
+    """
+    pass
